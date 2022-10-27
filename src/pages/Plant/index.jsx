7c4af530e1743eee54f966e68e746f34fe116911
Elsa David <els.david8@gmail.com>
2022-10-27 11:25:25 +0200

    Avancée page plante

--- src/pages/Plant/index.jsx
+++ src/pages/Plant/index.jsx
@@ -33,20 +33,16 @@ function Plant() {
           {tags.map((tag, index) => (
             <Tag key={`tag_${index}`}>{tag}</Tag>
           ))}
-        </div>
-
-        <div className="plant__head--params">
-                      <p>eau</p>
-                      <p>soleil</p>
-        </div>
+          </div>
+          <h2>( {selectedPlant.sciName} ) - {selectedPlant.origin}</h2>
       </div>
 
           <div className="plant__body">
-              <div className="plant__body--descri"><p>ARROSAGE //</p>
-                  <p>LUMINOSITE //</p>
-                  <p>SUBSTRAT //</p></div>
+              <div className="plant__body--descri"><p><h3>ARROSAGE //</h3><span> arrosage consignes</span></p>
+                  <p><h3>LUMINOSITE //</h3><span> luminosité consignes</span></p>
+                  <p><h3>SUBSTRAT //</h3><span> substrat consignes</span></p></div>
               
-<div className="plant__advice"><p></p></div>
+<div className="plant__advice"><p><h3>Les conseils de Bibou ...</h3> Danish tiramisu halvah tart macaroon cake pudding jelly-o tootsie roll. Sugar plum chupa chups gingerbread gummies cupcake jelly candy canes. Cake jelly sugar plum pastry gummies sesame snaps. Chocolate cake cotton candy gingerbread ice cream gummies bonbon shortbread oat cake. Caramels chupa chups lollipop marzipan bonbon sweet roll. Jelly-o pie bear claw gummi bears lollipop sweet jujubes topping tootsie roll. Tart candy canes sugar plum caramels chocolate bar tiramisu fruitcake. Macaroon croissant gummi bears candy canes sugar plum. Chocolate lollipop donut cheesecake muffin carrot cake. Oat cake sweet apple pie fruitcake marzipan macaroon. Chocolate cake sweet muffin pie lemon drops. Cookie gummies candy canes powder cupcake donut powder chocolate bar powder. Carrot cake pudding cookie pie halvah wafer tiramisu jelly beans jujubes.</p></div>
       </div>
     </div></div>
   );
--- src/utils/data/data.js
+++ src/utils/data/data.js
@@ -1,18 +1,24 @@
 export default [
 { id: '123',
-name: "Pilea",
+        name: "Pilea",
+        sciName: "Pilea peperomioides",
+origin: 'Asie',
 picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Chinese_money_plant.jpg/1200px-Chinese_money_plant.jpg",
 difficulty: 1,
 tags: ["plante d'intérieur", "plante verte"]
 },
 { id: '124',
-name: "Monstera",
+    name: "Monstera",
+    sciName: 'Monstera deliciosa',
+origin: 'Europe',
 picture: "https://www.ikea.com/fr/fr/images/products/monstera-plante-en-pot-monstera-deliciosa-faux-philo__0902106_pe640705_s5.jpg",
 difficulty: 2,
 tags: ["plante d'intérieur", "plante verte"]
     },
 { id: '125',
-name: "Oxalys",
+    name: "Oxalys",
+    sciName: 'Oxalis acetosella',
+    origin: 'Afrique',
 picture: "https://media.gerbeaud.net/2021/01/640/oxalis-triangularis-trefle-pourpre.jpg",
         difficulty: 3,
 tags: ["plante d'intérieur", "plante verte"]
